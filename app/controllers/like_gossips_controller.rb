a9264f0c39e42b8d48dbaf772bd9205aaeee60b5
Damien-Generet <damiengeneret@outlook.com>
2024-02-15 19:57:31 +0100

    ADD FUCKING, ENFIN, PUTAIN DE LIKE

--- app/controllers/like_gossips_controller.rb
+++ app/controllers/like_gossips_controller.rb
@@ -2,14 +2,12 @@ class LikeGossipsController < ApplicationController
   def create
     @gossip_to_like = Gossip.find(params[:gossip_id])
     @like_gossip = LikeGossip.create(user: current_user, gossip: @gossip_to_like)
-    puts "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
     redirect_to '/home'
   end
  
   def destroy
     @gossip_to_unlike = Gossip.find(params[:gossip_id])
-    @like_gossip = LikeGossip.find_by(user == current_user && gossip==@gossip_to_unlike).destroy
-    puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    @like_gossip = LikeGossip.find_by(user: current_user).destroy
     redirect_to '/home'
   end
  
